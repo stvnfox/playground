cdb8e71c1f1ee95e205301be26af95ff209a1c58
steven.vos <steven.vos@iodigital.com>
2025-03-05 15:53:56 +0100

    chore(old): remove old project

--- src/routes/index.lazy.tsx
+++
@@ -1,8 +0,0 @@
-import { Dashboard } from "@/components/pages/dashboard/Dashboard"
-import { createLazyFileRoute } from "@tanstack/react-router"
-
-export const Route = createLazyFileRoute("/")({
-    component: Dashboard,
-})
-
-
--- src/routes/navigation.lazy.tsx
+++
@@ -1,6 +0,0 @@
-import { Navigation } from "@/components/pages/navigation/Navigation"
-import { createLazyFileRoute } from "@tanstack/react-router"
-
-export const Route = createLazyFileRoute("/navigation")({
-    component: Navigation,
-})
